c25cafa909fa0573828cf0a1aeaeaa9d78a67cf0
chenyogie <chenyogie@gmail.com>
2019-07-06 19:05:05 +0800

    The employee entity controller.

--- src/main/java/com/yogie/web/controller/EmployeeController.java
+++ src/main/java/com/yogie/web/controller/EmployeeController.java
@@ -1,9 +1,16 @@
 package com.yogie.web.controller;
 
+import com.yogie.common.UIPage;
+import com.yogie.domain.Employee;
+import com.yogie.query.EmployeeQuery;
 import com.yogie.service.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @program: aimoll
@@ -12,10 +19,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Description:
  */
 @Controller
-@RequestMapping("employee")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
     private IEmployeeService employeeService;
+
+    @RequestMapping("/index")
+    public String index(){
+        return "employee/employee";
+    }
+
+    @RequestMapping("/findAll")
+    @ResponseBody
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    @RequestMapping("findPage")
+    @ResponseBody
+    public UIPage<Employee> findPage(EmployeeQuery baseQuery){
+        Page page = employeeService.findPageByQuery(baseQuery);
+        return new UIPage(page);
+    }
 
 }
